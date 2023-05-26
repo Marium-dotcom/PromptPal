c33199be7b1921ffdf9adcac8e5b63e978b7ba50
Marium-dotcom <74412502+Marium-dotcom@users.noreply.github.com>
2023-05-26 13:20:33 +0300

    edited display name

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PromptCard from './PromptCard'
 
-export default function ({data, handleDelete}) {
+export default function Profile ({data, handleDelete}) {
   return (
 
 <div className="p-5 md:p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
 
-{data.map((ppost) => <PromptCard ppost={ppost} handleDelete={() => handleDelete(ppost._id)}/>)
+{data.map((ppost,i) => <PromptCard key={i+1} ppost={ppost} handleDelete={() => handleDelete(ppost._id)}/>)
    
 }  
 </div>  
